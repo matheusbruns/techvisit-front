119764fc8b2d499419574010c573616be7e1428f
matheusbruns <matheus.rbruns@gmail.com>
2024-08-28 23:31:34 -0300

    TECH-7 feat: - adicionada biblioteca para notificações no sistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import AppRoutes from './router/Router';
 import { BrowserRouter } from 'react-router-dom';
 import "./util/styles/global.scss";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<AppRoutes />
+			<ToastContainer
+				position='top-right'
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
 		</BrowserRouter>
 	);
 }
